80108e353f8344162c805896172f235e2b8d871d
Denis Ilyushin <to@denisilyushin.ru>
2023-09-17 22:47:23 +0300

    feat: начал footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,18 @@
 import './Footer.css';
 
 function Footer() {
+  const currentYear = new Date().getFullYear()
+
   return (
-    <footer className='footer'>
+    <footer className="footer">
+      <h2 className={'footer__title'}> Учебный проект Яндекс.Практикум х BeatFilm.</h2>
+      <div className={'footer__content'}>
+        <p className={'footer__copyright'}>&copy; {currentYear}</p>
+        <div className={'footer__links'}>
+          <a className={'footer__link'}>Яндекс.Практикум</a>
+          <a className={'footer__link'}>Github</a>
+        </div>
+      </div>
     </footer>
   )
 }
