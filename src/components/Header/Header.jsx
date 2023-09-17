397e4c6bacf64e599d6e86a0f24b0ac4cfbc167e
Denis Ilyushin <to@denisilyushin.ru>
2023-09-17 22:16:41 +0300

    feat: сделать авторизованный хедер

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,45 +3,84 @@ import logo from '../../images/logo.svg';
 import {Link} from 'react-router-dom';
 
 function Header({authorized}) {
-  return (
-    (!authorized ? (
-      <header
-        id="header"
-        className={'header'}
-      >
-        <Link
-          to={'/'}
-          className={'header__logo'}
-        >
-          <img
-            src={logo}
-            alt={'логотип'}
-          />
-        </Link>
-        <div
-          className={'header__navigation'}
-        >
-          <Link
-            to={'/signup'}
-            className={'header__button'}
-          >
-            Регистрация
-          </Link>
-          <Link
-            to={'/signin'}
-            className={'header__button header__button_focused'}
-          >
-            Войти
-          </Link>
-        </div>
-      </header>
-    ) : (
-      <header
-        id="header"
-      >
 
-      </header>
-    ))
+  return (
+    !authorized
+      ? (
+        <header
+          id="header"
+          className={'header'}
+        >
+          <Link
+            to={'/'}
+            className={'header__logo'}
+          >
+            <img
+              src={logo}
+              alt={'логотип'}
+            />
+          </Link>
+          <div
+            className={'header__auth_navigation'}
+          >
+            <Link
+              to={'/signup'}
+              className={'header__auth_button'}
+            >
+              Регистрация
+            </Link>
+            <Link
+              to={'/signin'}
+              className={'header__auth_button header__auth_button_focused'}
+            >
+              Войти
+            </Link>
+          </div>
+        </header>
+      )
+      : (
+        <header
+          id="header"
+          className={'header'}
+        >
+          <Link
+            to={'/'}
+            className={'header__logo'}
+          >
+            <img
+              src={logo}
+              alt={'логотип'}
+            />
+          </Link>
+          <div
+            className={'header__app_navigation'}
+          >
+            <Link
+              to={'/movies'}
+              className={'header__app_nav_button'}
+            >
+              Фильмы
+            </Link>
+            <Link
+              to={'/saved-movies'}
+              className={'header__app_nav_button'}
+            >
+              Сохранённые фильмы
+            </Link>
+          </div>
+          <div
+            className={'header__auth_navigation'}
+          >
+            <Link
+              to={'/profile'}
+              className={'header__auth_nav_button'}
+            >
+              Аккаунт
+            </Link>
+          </div>
+        </header>
+      )
+
   )
 }
 
