2c4d16e667cc88486a57432553476e38fb2382c1
Denis Ilyushin <to@denisilyushin.ru>
2023-09-18 22:49:04 +0300

    feat: доделать футер

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -31,7 +31,7 @@ function App() {
             <Route
               path={'/'}
               element={
-                <Landing/>
+                <Landing />
               }
             />
           </Routes>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,12 +5,36 @@ function Footer() {
 
   return (
     <footer className="footer">
-      <h2 className={'footer__title'}> Учебный проект Яндекс.Практикум х BeatFilm.</h2>
-      <div className={'footer__content'}>
-        <p className={'footer__copyright'}>&copy; {currentYear}</p>
-        <div className={'footer__links'}>
-          <a className={'footer__link'}>Яндекс.Практикум</a>
-          <a className={'footer__link'}>Github</a>
+      <h2
+        className={'footer__title'}
+      >
+        Учебный проект Яндекс.Практикум х BeatFilm.
+      </h2>
+      <div
+        className={'footer__content'}
+      >
+        <p
+          className={'footer__copyright'}
+        >
+          &copy; {currentYear}
+        </p>
+        <div
+          className={'footer__links'}
+        >
+          <a
+            className={'footer__link'}
+            href={'https://practicum.yandex.ru'}
+            target={'_blank'}
+          >
+            Яндекс.Практикум
+          </a>
+          <a
+            className={'footer__link'}
+            href={'https://github.com/DenisIlyushin/movies-explorer-frontend'}
+            target={'_blank'}
+          >
+            Github
+          </a>
         </div>
       </div>
     </footer>
